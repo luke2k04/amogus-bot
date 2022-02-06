dcf225ef889e7fa1917980a0c4d2e87558d17546
luke2k04 <luke2k04@gmail.com>
2022-02-06 14:26:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
+
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.once("ready", () => {
+  console.log("Ready!");
+  client.user.setActivity("AMOGUS", { type: "PLAYING" });
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content.toLocaleLowerCase().search("sus") !== -1) {
+    message.channel.send("AMOGUS");
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
