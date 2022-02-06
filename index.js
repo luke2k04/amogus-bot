9e645d2ee4c1959b17f3a1cce1af9b271d7e3c47
luke2k04 <luke2k04@gmail.com>
2022-02-06 18:31:44 +0100

    added garnek feature

--- index.js
+++ index.js
@@ -11,8 +11,15 @@ client.once("ready", () => {
 });
 
 client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
   if (message.content.toLocaleLowerCase().search("sus") !== -1) {
     message.channel.send("AMOGUS");
+  } else if (message.content.toLocaleLowerCase().search("czarnek") !== -1) {
+    message.channel.send(
+      new Discord.MessageEmbed().setImage(
+        "https://cdn.discordapp.com/attachments/939843875776905236/939936244182437898/czarnek_ty_chuju.png"
+      )
+    );
   }
 });
 
